88f538f51b2cedfd4e4c64db72c610bc149d05c9
Dhi Carvalho <dhi.carvalho85@gmail.com>
2021-06-28 10:52:27 -0300

    Completion of NLW Together Mission: Origin

--- main.js
+++ main.js
@@ -15,10 +15,12 @@ for (const link of links) {
   })
 }
 
-function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+const backToTopButton = document.querySelector('.back-to-top')
+const sections = document.querySelectorAll('main section[id]')
 
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
@@ -27,8 +29,6 @@ function changeHeaderWhenScroll() {
 }
 
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
-
   if (window.scrollY >= 780) {
     backToTopButton.classList.add('show')
   } else {
@@ -36,9 +36,32 @@ function backToTop() {
   }
 }
 
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
 window.addEventListener('scroll', () => {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSection()
 })
 
 const swiper = new Swiper('.swiper-container', {
@@ -47,7 +70,13 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 const scrollReveal = ScrollReveal({
